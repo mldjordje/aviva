df31422eefb5468710bc19bd91f9da4d78d15521
mldjordje <165197022+mldjordje@users.noreply.github.com>
2025-10-28 14:33:25 +0100

    Perf: add font-display: swap for local fonts; disable legacy browsers transpilation and enable modern image formats

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  swcMinify: true,
+  experimental: {
+    legacyBrowsers: false,
+  },
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
 };
 
 export default nextConfig;
