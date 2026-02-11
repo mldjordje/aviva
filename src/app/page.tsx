ee10840ae0709aca2e0126fda5858217caabb791
mldjordje <165197022+mldjordje@users.noreply.github.com>
2026-02-11 18:02:10 +0100

    Vracen landing page i uklonjeni clanovi iz Nas tim

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,37 @@
 import React from "react";
-import Image from "next/image";
+import HeaderOne from "@/layouts/headers/HeaderOne";
+import HeroHomeOne from "@/components/hero/HeroHomeOne";
+import HeroHomeThree from "@/components/hero/HeroHomeThree";
+import AboutHomeOne from "@/components/about/AboutHomeOne";
+import ServiceHomeOne from "@/components/service/ServiceHomeOne";
+import PortfolioHomeOne from "@/components/portfolio/PortfolioHomeOne";
+import FunFactHomeOne from "@/components/funfact/FunFactHomeOne";
+import VideoHomeOne from "@/components/video/VideoHomeOne";
+import BrandHomeOne from "@/components/brand/BrandHomeOne";
+import FooterOne from "@/layouts/footers/FooterOne";
 import Wrapper from "@/layouts/Wrapper";
-import logo from "@/assets/img1/logo4.png";
-import { CONTACT } from "@/data/contact";
+import Cenovnik from "@/components/cenovnik/Cenovnik";
+import TeamArea from "@/components/team/TeamArea";
 
 const IndexPage = () => {
   return (
     <Wrapper>
+      <HeaderOne />
       <div id="smooth-wrapper">
         <div id="smooth-content">
           <main>
-            <section>
-              <div className="cs_hero cs_style1 cs_center">
-                <div
-                  className="cs_hero_bg cs_bg"
-                  style={{ backgroundImage: "url(/assets/img1/4.jpg)" }}
-                ></div>
-                <div className="container">
-                  <div className="cs_hero_text">
-                    <div className="cs_hero_text_in">
-                      <Image
-                        src={logo}
-                        alt="Aviva Ageless logo"
-                        width={160}
-                        height={60}
-                        priority
-                      />
-                    </div>
-                    <div className="cs_height_20 cs_height_lg_20"></div>
-                    <div className="cs_hero_mini_title cs_hero_text_in">
-                      Aviva Ageless
-                    </div>
-                    <div className="cs_height_20 cs_height_lg_20"></div>
-                    <h1 className="cs_hero_title cs_hero_text_in">Uskoro</h1>
-                    <div className="cs_height_40 cs_height_lg_30"></div>
-                    <div className="cs_hero_text_in">
-                      <div className="cs_hero_subtitle">
-                        Pripremamo novu web stranicu. Za zakazivanje i informacije, kontaktirajte nas.
-                      </div>
-                      <div className="cs_height_40 cs_height_lg_30"></div>
-                      <div className="cs_btn cs_style_2">
-                        <a href={`tel:${CONTACT.phone}`} className="cs_center">Pozovi</a>
-                        <a href={`mailto:${CONTACT.email}`} className="cs_center">Email</a>
-                      </div>
-                      <div className="cs_height_30 cs_height_lg_20"></div>
-                      <div className="cs_hero_subtitle">
-                        {CONTACT.address.streetAddress}, {CONTACT.address.addressLocality}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </section>
+            <HeroHomeOne />
+            <HeroHomeThree />
+            <AboutHomeOne />
+            <ServiceHomeOne />
+            <Cenovnik />
+            <PortfolioHomeOne />
+            <FunFactHomeOne />
+            <VideoHomeOne />
+            <TeamArea />
+            <BrandHomeOne style_2={true} />
           </main>
+          <FooterOne />
         </div>
       </div>
     </Wrapper>
--- src/data/team_data.ts
+++ src/data/team_data.ts
@@ -1,6 +1,4 @@
 import team_avatar_1 from "@/assets/img1/mila1.png";
-import team_avatar_2 from "@/assets/img/teamsimg2.jpg";
-import team_avatar_3 from "@/assets/img/teamsimg3.jpg";
 import team_avatar_4 from "@/assets/img/teamsimg4.jpg";
 
 import { StaticImageData } from "next/image";
@@ -23,20 +21,6 @@ const team_data: DataType[] = [
   },
   {
     id: 2,
-    img: team_avatar_2,
-    avatar_name: "Dr Kristina Stojanović",
-    designation: "Estetski hirurg",
-    link: "/nas-tim/Kristina",
-  },
-  {
-    id: 3,
-    img: team_avatar_3,
-    avatar_name: "Dr Nikola Igić",
-    designation: "Estetski hirurg",
-    link: "/nas-tim/Nikola",
-  },
-  {
-    id: 4,
     img: team_avatar_4,
     avatar_name: "Dr Marša Leone Papović",
     designation: "Estetski hirurg",
